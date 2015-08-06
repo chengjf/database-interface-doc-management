799d5da7696452d8c67c59955c9afc3164a96b3f
chengjf <sharpkingdom@gmail.com>
2015-08-06 22:49:16 +0800

    Commit app system edit function.

--- JFinalDemo/src/com/chengjf/jfinaldemo/Application.java
+++ JFinalDemo/src/com/chengjf/jfinaldemo/Application.java
@@ -7,7 +7,7 @@ public class Application {
 	public static void main(String[] args) {
 		
 		
-		JFinal.start("webapp", 8080, "/", 10);
+		JFinal.start("webapp", 8888, "/", 10);
 	}
 	
 }
--- JFinalDemo/src/com/chengjf/jfinaldemo/controller/AppSystemController.java
+++ JFinalDemo/src/com/chengjf/jfinaldemo/controller/AppSystemController.java
@@ -7,7 +7,6 @@ import org.apache.log4j.Logger;
 import com.chengjf.jfinaldemo.common.Constants;
 import com.chengjf.jfinaldemo.interceptor.UserInterceptor;
 import com.chengjf.jfinaldemo.model.AppSystem;
-import com.chengjf.jfinaldemo.model.User;
 import com.jfinal.aop.Before;
 
 public class AppSystemController extends CommonController {
@@ -17,27 +16,39 @@ public class AppSystemController extends CommonController {
 
 	@Before(UserInterceptor.class)
 	public void index() {
-		String method = getRequest().getMethod();
 		List<AppSystem> systemList = AppSystem.me.getAllAppSystems();
 		setAttr("systemList", systemList);
 		render("index.html");
 	}
 
+	public void find() {
+		try {
+			String id = getPara("id");
+			AppSystem system = AppSystem.me.getAppSystemById(id);
+			setAttr("system", system);
+			success(system);
+		} catch (Exception e) {
+			error(Constants.FIND_FAILURE);
+		}
+	}
+
+	@Before(UserInterceptor.class)
 	public void delete() {
 		String method = getRequest().getMethod();
 
 		if (method.equalsIgnoreCase(Constants.RequestMethod.POST)) {
 			try {
-				String delete_id = getPara("delete_id");
+				String delete_id = getPara("id");
+				logger.info("Delete AppSystem id=" + delete_id);
 				AppSystem.me.deleteById(delete_id);
 				success();
 			} catch (Exception e) {
 				error(Constants.DELETE_FAILURE);
 			}
 		}
-		// redirect(Constants.getBaseUrl() + "/system");
 	}
 
+	@Before(UserInterceptor.class)
 	public void add() {
 		String method = getRequest().getMethod();
 		if (method.equalsIgnoreCase(Constants.RequestMethod.POST)) {
@@ -50,6 +61,27 @@ public class AppSystemController extends CommonController {
 					.set(AppSystem.SYSTEM_DESC, desc).save();
 		}
 		redirect(Constants.getBaseUrl() + "/system");
+	}
+
+	@Before(UserInterceptor.class)
+	public void edit() {
+		String method = getRequest().getMethod();
+		if (method.equalsIgnoreCase(Constants.RequestMethod.POST)) {
+			try {
+				AppSystem system = getModel(AppSystem.class, "system");
+
+				if (system.update()) {
+					logger.info("Saved " + system.getStr(AppSystem.SYSTEM_ID));
+					success("保存成功！");
+				} else {
+					error(Constants.EDIT_FAILURE);
+				}
+
+			} catch (Exception e) {
+				logger.error(e);
+				error(Constants.EDIT_FAILURE);
+			}
+		}
 	}
 
 }
